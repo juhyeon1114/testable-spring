58a54cb6e37eb37299fa204b708ab33ab0aa64ba
juhyeon <juhyeon1114@gmail.com>
2024-07-01 22:47:40 +0900

    init

---
+++ src/main/java/study/testable_spring/TestableSpringApplication.java
@@ -0,0 +1,13 @@
+package study.testable_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestableSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestableSpringApplication.class, args);
+	}
+
+}
